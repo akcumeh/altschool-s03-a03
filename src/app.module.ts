0dda7dcfd35f37fccefad7f9dd33fbf76d0dd17f
Angel Umeh <akcumeh@gmail.com>
2026-05-19 23:38:26 +0100

    Set up Supabase

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { SupabaseModule } from './supabase/supabase.module';
+import { SessionModule } from './session/session.module';
+import { MenuModule } from './menu/menu.module';
+import { OrdersModule } from './orders/orders.module';
+import { PaymentModule } from './payment/payment.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
-    imports: [ConfigModule.forRoot({ isGlobal: true })],
+    imports: [ConfigModule.forRoot({ isGlobal: true }), SupabaseModule, SessionModule, MenuModule, OrdersModule, PaymentModule, ChatModule],
 })
 export class AppModule { }
